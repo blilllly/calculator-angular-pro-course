a4e318e01ca490d60f5f5746a2cccbf4f01a2137
Billy Alvear <alvearbilly@gmail.com>
2025-10-08 06:54:15 -0500

    first tests

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,25 +1,39 @@
 import { provideZonelessChangeDetection } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
+  let fixture: ComponentFixture<App>;
+  let compiled: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-      providers: [provideZonelessChangeDetection()]
+      providers: [provideZonelessChangeDetection()],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(App);
+    compiled = fixture.nativeElement as HTMLElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
+  it('should render router-outlet', () => {
+    expect(compiled.querySelector('router-outlet')).not.toBeNull();
+  });
+
+  it('should be 3', () => {
+    // A = Arrange - Arreglar
+    const num1 = 1;
+    const num2 = 2;
+
+    // A = Act - Actuar
+    const result = num1 + num2;
+
+    // A = Assert - Afirmar
+    expect(result).toBe(3);
   });
 });
