f36d5dd794e54b920c67b6d995170ed18b2ec24c
Billy Alvear <alvearbilly@gmail.com>
2025-10-16 06:39:42 -0500

    end of first test

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -36,4 +36,30 @@ describe('App', () => {
     // A = Assert - Afirmar
     expect(result).toBe(3);
   });
+
+  it('should render router-outlet wrapped with css classes', () => {
+    const divElement = compiled.querySelector('div');
+    const mustHaveClasses =
+      'min-w-screen min-h-screen bg-slate-500 flex items-center justify-center px-5 py-5'.split(
+        ' '
+      );
+
+    expect(divElement).not.toBeNull();
+
+    // divElement?.classList.forEach((className) => {
+    //   expect(mustHaveClasses).toContain(className);
+    // });
+
+    const divClasses = divElement?.classList.value.split(' ');
+
+    mustHaveClasses.forEach((className) => {
+      expect(divClasses).toContain(className);
+    });
+  });
+
+  // it("should contain the 'buy me a beer' link", () => {
+  //   const anchorElement = compiled.querySelector('a');
+  //   expect(anchorElement).not.toBeNull();
+  //   expect(anchorElement?.href).toBe('link');
+  // });
 });
