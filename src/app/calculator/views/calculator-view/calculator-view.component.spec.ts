86c8dd4cdb4632ee93597513e51a9076b028c985
Billy Alvear <alvearbilly@gmail.com>
2025-10-20 06:11:54 -0500

    calculator-view-test

---
+++ src/app/calculator/views/calculator-view/calculator-view.component.spec.ts
@@ -0,0 +1,41 @@
+import { provideZonelessChangeDetection } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import CalculatorViewComponent from './calculator-view.component';
+
+describe('CalculatorViewComponent', () => {
+  let fixture: ComponentFixture<CalculatorViewComponent>;
+  let compiled: HTMLElement;
+  let component: CalculatorViewComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CalculatorViewComponent],
+      providers: [provideZonelessChangeDetection()],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CalculatorViewComponent);
+    compiled = fixture.nativeElement as HTMLElement;
+    component = fixture.componentInstance;
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should containt calculator component', () => {
+    expect(compiled.querySelector('calculator')).not.toBeNull();
+  });
+
+  it('should contain basic css classes', () => {
+    const divElement = compiled.querySelector('div');
+    const mustHaveClasses =
+      'w-full mx-auto rounded-xl bg-gray-100 shadow-xl text-gray-800 relative overflow-hidden'.split(
+        ' '
+      );
+    const divClasses = divElement?.classList.value.split(' ');
+
+    mustHaveClasses.forEach((className) => {
+      expect(divClasses).toContain(className);
+    });
+  });
+});
